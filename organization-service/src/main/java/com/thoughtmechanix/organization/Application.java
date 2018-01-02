a352966d4c47d7c3c9216861804cb5cbc95d9d4f
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2018-01-02 14:37:21 +0800

    ...

--- organization-service/src/main/java/com/thoughtmechanix/organization/Application.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Created by gongzhaopeng on 08/12/2017.
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
