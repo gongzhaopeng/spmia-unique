839da023618ca4c5254a79502d5994f5db63ef24
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2018-01-07 00:16:27 +0800

    ...

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region ap-northeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
+ecs-cli configure profile --profile-name base --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure --cluster spmia-tmx-dev --region ap-northeast-2 --config-name base
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
